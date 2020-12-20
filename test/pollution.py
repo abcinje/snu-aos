485f64a7947fca327f0c311793f45a7f5c27a467
Injae Kang <abcinje@gmail.com>
2020-12-21 04:02:59 +0900

    Update pollution.py

--- test/pollution.py
+++ test/pollution.py
@@ -2,6 +2,7 @@ import numpy as np
 import random
 import os
 import re
+import time
 
 # This 5000 x 5000 matrix occupies 334MB of memory
 matrix = np.random.randn(5000, 5000)
@@ -11,3 +12,4 @@ while True:
     x = random.randint(0, 4999)
     y = random.randint(0, 4999)
     summation += matrix[x][y]
+    time.sleep(0.1)
